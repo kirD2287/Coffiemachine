323c3fccc860d50b27c34d4214563096784d57ac
Kirill Dyachkov <dj_pro98@mail.ru>
2024-03-03 16:06:37 +0600

    основной код для работы

---
+++ coffiemachine class code.js
@@ -0,0 +1,130 @@
+class CoffieMachine {
+    constructor(brand, waterСapacity, beanCapacity) {
+        this.brand = brand 
+        this.waterСapacity = waterСapacity,
+        this.beanCapacity = beanCapacity,
+        this.waterAmount =  this.waterСapacity
+        this.beanAmount = this.beanCapacity
+        
+    }
+
+    upvote () {
+        this.waterAmount = this.waterСapacity
+        console.log('Бак для воды полный')         // ЗАПОЛНИ БАК С ВОДОЙ
+    }
+    upvote1 () {
+        this.beanAmount =  this.beanCapacity
+        console.log('Зёрна засыпаны')               // ЗАПОЛНИ БАК С КОФЕ
+    }
+   downvote () {
+    this.waterAmount = 0
+    console.log('Заполните бак')
+   }
+   downvote1 () {
+    this.beanAmount =  this.beanCapacity
+    console.log('Зёрен нет, попытайтесь не нервничать')
+    
+}
+    showCurrWatAndCoffAmount () {
+       console.log(`${this.waterAmount}  ${this.beanAmount}`) // ПОКАЗЫВАТЬ ТЕКУЩИЙ ОБЪЕМ ВОДЫ И КОФЕ
+
+}  
+    makeAmericano() {
+    const recepteAmericano = new RecepteAmericano() // ПРИГОТОВЬ АМЕРИКАНО
+    if (recepteAmericano.beans > this.beanAmount && recepteAmericano.water > this.waterAmount) {
+        console.log('Недостаточно ингредиентов')
+        return
+    }
+    console.log('Делается...')
+    setTimeout(() => console.log('Готово'), 7000)
+    return
+}
+    makeEspresso() {
+    const recepteEspresso = new RecepteEspresso() // ПРИГОТОВЬ ЭСПРЕССО
+    if (recepteEspresso.beans > this.beanAmount && recepteEspresso.water > this.waterAmount) {
+        console.log('Недостаточно ингредиентов')
+        return
+    }
+    console.log('Делается...')
+    setTimeout(() => console.log('Готово'), 10000)
+    return
+}
+    makeDoubleEspresso() {
+    const recepteDoubleEspresso = new RecepteDoubleEspresso() // ПРИГОТОВЬ ДВОЙНОЙ ЭСПРЕССО
+    if (recepteDoubleEspresso.beans > this.beanAmount && recepteDoubleEspresso.water > this.waterAmount) {
+        console.log('Недостаточно ингредиентов')
+        return
+    }
+    console.log('Делается...')
+    setTimeout(() => console.log('Готово'), 5000)
+    return
+}
+    getInformation () {                                                 // ВЫВОД ИНФОРМАЦИИ
+    return {brand: this.brand,
+        waterСapacity: this.waterСapacity,
+        beanCapacity: this.beanCapacity,
+        
+    } 
+}
+    offMachine () {                                                    //  Выключи кофемашину
+
+    }
+}
+
+  class ThompsonMachine extends CoffieMachine {
+    constructor(){
+         super('Thompson', 1200, 150)
+    }
+}
+
+class GrundigMachine extends CoffieMachine {
+    constructor(){
+         super('Grundig', 1500, 160)
+    }
+}
+
+class PhillipsMachine extends CoffieMachine {
+    constructor(){
+         super('Phillips', 1800, 275)
+    }
+}
+
+class ReceptCoffie {
+    constructor(beans, water) {
+        this.beans = beans
+        this.water = water
+    }
+   
+}
+
+ class RecepteAmericano extends ReceptCoffie {
+    constructor(){
+         super(30, 200)
+    
+    }
+}
+
+ class RecepteEspresso extends ReceptCoffie {
+    constructor(){
+         super(50, 300)
+    
+    } 
+}
+
+ class RecepteDoubleEspresso  extends ReceptCoffie {
+    constructor(){
+         super(70, 400)
+         
+    }
+}
+
+    export {
+        CoffieMachine,
+        ThompsonMachine,
+        GrundigMachine,
+        PhillipsMachine,
+        ReceptCoffie,
+        RecepteAmericano,
+        RecepteEspresso,
+        RecepteDoubleEspresso
+    }
